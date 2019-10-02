b2579016de645eb63388fcc0bb9bd1f6b53f0c32
Christoffer Bengts <christoffer.bengts@gmail.com>
2019-10-02 19:22:43 +0200

    Done

--- addBorder/addBorder.js
+++ addBorder/addBorder.js
@@ -1,32 +1,21 @@
-function addBorder(picture: string[]): string[] {
-    
-    let longestItem = 0;
-
-    let retArr = picture.map(function (i:string) {
+function addBorder(picture) {
+    var longestItem = 0;
+    var retArr = picture.map(function (i) {
         if (i.length > longestItem) {
             longestItem = i.length;
         }
         return "*" + i + "*";
-    })
-
-    let firstLastRow = "**";
-
-    for (let i = 0; i < longestItem; i++) {
+    });
+    var firstLastRow = "**";
+    for (var i = 0; i < longestItem; i++) {
         firstLastRow += "*";
-        
     }
-
     retArr.unshift(firstLastRow);
     retArr.push(firstLastRow);
-    
     return retArr;
-    
 }
-
 console.log(addBorder(["asf", "afeafe", "af", "lowkoflawf"]));
-
 console.log(addBorder(["abc", "ded"]));
-
 // Find largest item in array
 /*function largestItem(mainArr: string[]) {
     let subArr = mainArr.map(function (elem: string) {
@@ -37,4 +26,4 @@ console.log(addBorder(["abc", "ded"]));
     return subArr.reduce(function (prevLargest: number, currentLargest: number) {
         return (currentLargest > prevLargest) ? currentLargest : prevLargest;
     })
-}*/
+}*/ 
