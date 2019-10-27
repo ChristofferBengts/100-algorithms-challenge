40f89ac0630f57488b754af024049fc12e7d4a32
Christoffer Bengts <christoffer.bengts@gmail.com>
2019-10-27 13:52:55 +0100

    Done

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,6 +1,6 @@
 function alphabetSubsequence(s: string): boolean {
     let result: boolean = true;
-    let stringArr = s.split('');
+    let stringArr: string[] = s.split('');
 
     for (let i = 1; i < stringArr.length; i++) { 
         if (!(stringArr[i].charCodeAt(0) > stringArr[i-1].charCodeAt(0))) {         
