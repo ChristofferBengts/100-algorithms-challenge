a747cc0919ade881407cb03332c55ebf68eb9c16
Christoffer Bengts <christoffer.bengts@gmail.com>
2019-11-03 20:00:40 +0100

    Started

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -8,6 +8,8 @@ function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: numb
     return result;
 }
 
+// use switch statement instead?
+
 console.log(areEquallyStrong(10, 15, 15, 10))
 console.log(areEquallyStrong(15, 10, 15, 10))
 console.log(areEquallyStrong(15, 10, 15, 9))
