97444b875013589b71d7b68d6e66ab3bc1070d3e
Christoffer Bengts <christoffer.bengts@gmail.com>
2019-11-07 20:48:10 +0100

    Done

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,17 +1,11 @@
 function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
 
-    let result: boolean = false;
-
     let yourStrongest: number = (Math.max(yourLeft, yourRight));
     let friendsStrongest: number = (Math.max(friendsLeft, friendsRight));
     let yourWeakest: number = (Math.min(yourLeft, yourRight));
     let friendsWeakest: number = (Math.min(friendsLeft, friendsRight));
 
-    if (yourStrongest === friendsStrongest && yourWeakest === friendsWeakest) {
-        return true;
-    };
-
-    return result;
+    return yourStrongest === friendsStrongest && yourWeakest === friendsWeakest;
 }
 
 console.log(areEquallyStrong(10, 15, 15, 10))
--- areEquallyStrong/areequallystrong.js
+++ areEquallyStrong/areequallystrong.js
@@ -1,14 +1,9 @@
 function areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight) {
-    var result = false;
     var yourStrongest = (Math.max(yourLeft, yourRight));
     var friendsStrongest = (Math.max(friendsLeft, friendsRight));
     var yourWeakest = (Math.min(yourLeft, yourRight));
     var friendsWeakest = (Math.min(friendsLeft, friendsRight));
-    if (yourStrongest === friendsStrongest && yourWeakest === friendsWeakest) {
-        return true;
-    }
-    ;
-    return result;
+    return yourStrongest === friendsStrongest && yourWeakest === friendsWeakest;
 }
 console.log(areEquallyStrong(10, 15, 15, 10));
 console.log(areEquallyStrong(15, 10, 15, 10));
