1c3aafbadd16d3fb615e70c891137ae55a07ecdd
Christoffer Bengts <christoffer.bengts@gmail.com>
2019-11-08 21:53:48 +0100

    Started

--- areSimilar/areSimilar.ts
+++ areSimilar/areSimilar.ts
@@ -1,7 +1,29 @@
 function areSimilar(a: number[], b: number[]): boolean {
    
+    let bool = false;
+        
+        if (a.join('') === b.join('')) {
+            bool = true;
+        } else {
+            for (let i = 0; i < a.length; i++) {             
+                let newB = [...b];
+
+                newB[i] = a[i];
+
+                if (a.join('') === newB.join('')) {
+                    bool = true;
+                    break;
+                }    
+            }
+        }
+    
+
+    return  bool;
 }
 
 console.log(areSimilar([1, 2, 3], [1, 2, 3]));
 console.log(areSimilar([1, 2, 3], [2, 1, 3]));
 console.log(areSimilar([1, 2, 2], [2, 1, 1]));
+console.log(areSimilar([3, 2, 1], [2, 2, 1]));
+
+// Wrong solution, needs to swap values instead of replacing
