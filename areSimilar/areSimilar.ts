1345f85e24d7001d8c442537af3b9acd3a3cd4e2
Christoffer Bengts <christoffer.bengts@gmail.com>
2019-11-14 00:46:01 +0100

    Done

--- areSimilar/areSimilar.ts
+++ areSimilar/areSimilar.ts
@@ -5,52 +5,35 @@ function areSimilar(a: number[], b: number[]): boolean {
         if (a.join('') === b.join('')) {
             bool = true;
         } else {
-            for (let i = 0; i < a.length; i++) {             
-                let newB = [...b];
-                let newA = [...a];
+            // Swap every element in array with every other element in array once
+            for (let i = 0; i < b.length; i++) {             
+                let cur = i;
 
-                // Swap
+                for (let i = 0; i < b.length; i++) {             
+                    let newB = [...b];
+                    // Swap        
+                    newB[cur] = b[b.length - (i + 1)];
+                    newB[b.length - (i + 1)] = b[cur];
+                    // Check if same
+                    if (newB.join('') === a.join('')) {
+                        bool = true;
+                        break;
+                    }    
+                }
 
-                // 1. element 0 <> element 1
-                newB[i] = b[i+1];
-                newB[i+1] = b[i];
-                if (newB.join('') === a.join('')) {
-                    bool = true;
+                if (bool) {
                     break;
-                }    
-                // 2. element 0 <> element 2
-                newB = [...b];
-                newB[i] = b[i+2];
-                newB[i+2] = b[i];
-                if (newB.join('') === a.join('')) {
-                    bool = true;
-                    break;
-                }    
-                // 3. element 1 <> element 2
-                newB = [...b];
-                newB[i+1] = b[i+2];
-                newB[i+2] = b[i+1];
-                if (newB.join('') === a.join('')) {
-                    bool = true;
-                    break;
-                }    
-
-
-                //newA[i] = a[i+1];
-                // should be something like newB[i] = newB.length-i
-
-                console.log(newA);
-                console.log(newB);
+                }
             }
         }
-    
-
+        
     return  bool;
 }
 
-//console.log(areSimilar([1, 2, 3], [1, 2, 3]));
+console.log(areSimilar([1, 2, 3], [1, 2, 3]));
 console.log(areSimilar([1, 2, 3], [2, 1, 3]));
-//console.log(areSimilar([1, 2, 2], [2, 1, 1]));
-//console.log(areSimilar([3, 2, 1], [2, 2, 1]));
-
-// Wrong solution, needs to swap values instead of replacing
+console.log(areSimilar([1, 2, 2], [2, 1, 1]));
+console.log(areSimilar([3, 2, 1], [2, 2, 1]));
+console.log(areSimilar([3, 2, 2], [2, 2, 3]));
+console.log(areSimilar([1, 4, 3, 2, 5], [1, 2, 3, 4, 5]));
+console.log(areSimilar([1, 2, 7, 4, 5, 6, 3, 8, 9, 10], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
---
+++ areSimilar/aresimilar.js
@@ -0,0 +1,41 @@
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
+function areSimilar(a, b) {
+    var bool = false;
+    if (a.join('') === b.join('')) {
+        bool = true;
+    }
+    else {
+        // Swap every element in array with every other element in array once
+        for (var i = 0; i < b.length; i++) {
+            var cur = i;
+            for (var i_1 = 0; i_1 < b.length; i_1++) {
+                var newB = __spreadArrays(b);
+                // Swap        
+                newB[cur] = b[b.length - (i_1 + 1)];
+                newB[b.length - (i_1 + 1)] = b[cur];
+                // Check if same
+                if (newB.join('') === a.join('')) {
+                    bool = true;
+                    break;
+                }
+            }
+            if (bool) {
+                break;
+            }
+        }
+    }
+    return bool;
+}
+console.log(areSimilar([1, 2, 3], [1, 2, 3]));
+console.log(areSimilar([1, 2, 3], [2, 1, 3]));
+console.log(areSimilar([1, 2, 2], [2, 1, 1]));
+console.log(areSimilar([3, 2, 1], [2, 2, 1]));
+console.log(areSimilar([3, 2, 2], [2, 2, 3]));
+console.log(areSimilar([1, 4, 3, 2, 5], [1, 2, 3, 4, 5]));
+console.log(areSimilar([1, 2, 7, 4, 5, 6, 3, 8, 9, 10], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
